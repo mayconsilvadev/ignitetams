315e9c10187dc41977405843dde4aaee9264e5c9
mayconsilva24 <sousamaycon83@gmail.com>
2024-12-22 17:15:25 -0300

    Initial project

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Container, Title } from './styles';
+
+
+export function Groups() {
+  return (
+    <Container>
+      <Title>
+        Groups
+      </Title>
+    </Container>
+  );
+}
+
+
