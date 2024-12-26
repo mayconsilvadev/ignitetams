24937b88f70cb31697a3f0475ee15cace3215a6f
mayconsilva24 <sousamaycon83@gmail.com>
2024-12-26 00:56:18 -0300

    creating the components folder

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard';
 
-import { Container, Title } from './styles';
-
+import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
+      <Header />
+
+      <Highlight 
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+      <GroupCard title='Maycon'/>
     </Container>
   );
 }
