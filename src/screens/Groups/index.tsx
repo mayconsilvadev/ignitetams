543d6070d95e5f0ef824f5435c04ad5e32dc48fd
mayconsilva24 <sousamaycon83@gmail.com>
2025-01-05 01:01:33 -0300

    playercard creation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight'
-import { GroupCard } from '@components/GroupCard';
+import { useState,  } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container } from './styles';
 
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+
+
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Maycon']);
+
   return (
     <Container>
       <Header />
@@ -13,6 +20,23 @@ export function Groups() {
         title='Turmas'
         subtitle='Jogue com a sua turma'
       />
+
+      <FlatList
+       data={groups}
+       keyExtractor={item => item}
+       renderItem={({ item }) => (
+        <GroupCard 
+           title={item}
+        />
+       )}
+       contentContainerStyle={groups.length === 0 && { flex: 1 }}
+       ListEmptyComponent={() => (
+        <ListEmpty
+          message="Que tal cadastrar a primeira turma?"
+        />
+       )}
+      />
+
       <GroupCard title='Maycon'/>
     </Container>
   );
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
-import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
-
+import { Button } from '@components/Button';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
-import { Filter } from '@components/Filter';
+import { ListEmpty } from '@components/ListEmpty';
+import { PlayerCard } from '@components/PlayerCard';
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 export function Players(){
     const [team, setTeam] = useState('Time A');
@@ -52,6 +55,31 @@ export function Players(){
               </NumbersOfPlayers>
             </HeaderList>
 
+            <FlatList
+             data={players}
+             keyExtractor={item => item}
+             renderItem={({ item }) => (
+              <PlayerCard 
+               name={item}
+               onRemove={() => {}}
+              />
+             )}
+              ListEmptyComponent={() => (
+                <ListEmpty
+                  message="Não há pessoas nesse time."
+                />
+              )}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={[
+                {paddingBottom: 100},
+                players.length === 0 && {flex: 1}
+              ]}
+            />
+
+            <Button
+             title="Remover Turma"
+             type="SECONDARY"
+            />
         </Container>
     );
 }
