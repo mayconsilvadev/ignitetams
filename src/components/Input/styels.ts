fcd8e15fad68cfc2b92ec20a66eed9b979225832
mayconsilva24 <sousamaycon83@gmail.com>
2025-01-04 23:58:29 -0300

    update to the players and components file

--- src/components/Input/styels.ts
+++ src/components/Input/styels.ts
@@ -5,7 +5,7 @@ import { TextInput } from 'react-native';
 export const Container = styled(TextInput)`
   flex: 1;
   min-height: 56px;
-  max-height: 56px;
+  max-height: 5px;
   ${({ theme }) => css`
     background-color: ${theme.COLORS.GRAY_700};
     color: ${theme.COLORS.WHITE};
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,7 @@
-import { Container, Form } from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -7,6 +10,9 @@ import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 
 export function Players(){
+    const [team, setTeam] = useState('Time A');
+    const [players, setPlayers] = useState([]);
+
     return(
         <Container>
             <Header showBackButton />
@@ -27,10 +33,25 @@ export function Players(){
                 />
             </Form>
 
-            <Filter
-             title="Time A"
-             isActive
-            />
+            <HeaderList>
+              <FlatList
+                data={['Time  A', 'Time  B']}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                  <Filter
+                    title={item}
+                    isActive={item === team}
+                    onPress={() => setTeam(item)}
+                  />
+                )}
+                horizontal
+              />
+
+              <NumbersOfPlayers>
+                {players.length}
+              </NumbersOfPlayers>
+            </HeaderList>
+
         </Container>
     );
 }
